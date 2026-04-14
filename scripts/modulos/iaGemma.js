527a0e85085932b5ae00b7a8acb4fbbd02049948
Noelia Pires <marianoeliapires@gmail.com>
2026-04-14 10:34:27 +0200

    cocheIA

--- scripts/modulos/iaGemma.js
+++ scripts/modulos/iaGemma.js
@@ -1,23 +1,4 @@
 export async function decidirMovimientoIA(estado) {
-
-    try {
-        const response = await fetch("http://localhost:3000/ia", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                playerX: estado.playerX,
-                iaX: estado.iaX
-            })
-        });
-
-        const data = await response.json();
-
-        return data.decision.trim();
-
-    } catch (error) {
-        console.error("Error IA:", error);
-        return "recto"; // fallback para que no se rompa
-    }
+    // IA simple para producción (GitHub Pages)
+    return "recto";
 }
